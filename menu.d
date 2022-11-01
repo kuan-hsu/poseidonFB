bf0a87ababf98239e1ddf18ba6f9f814a6cdb9c7
Kuan Hsu <nagahiro.kyo@gmail.com>
2022-11-01 20:17:39 +0800

    V0.502: Fixed "HTML APP Path" not work at menu item bug (Linux only)

--- menu.d
+++ menu.d
@@ -728,7 +728,7 @@ void createMenu()
 	IupSetAttribute(item_about, "IMAGE", "icon_information");
 	IupSetCallback( item_about, "ACTION", cast(Icallback) function( Ihandle* ih )
 	{
-		version(FBIDE)	IupMessage( GLOBAL.languageItems["about"].toCString, toStringz( "FreeBasic IDE\nPoseidonFB(V0.501)  2022.10.30\nBy Kuan Hsu (Taiwan)\nhttps://bitbucket.org/KuanHsu/poseidonfb\n\nlibreoffice-style-sifr ICONs\nBy Rizal Muttaqin\nhttps://github.com/rizmut/libreoffice-style-sifr\n" ~ ( GLOBAL.linuxHome.length ? "\nAppImage" : ""  ~ ( GLOBAL.iconv != null ? "\n*Using iconv Library" : "" ) ) ) );
+		version(FBIDE)	IupMessage( GLOBAL.languageItems["about"].toCString, toStringz( "FreeBasic IDE\nPoseidonFB(V0.502)  2022.11.01\nBy Kuan Hsu (Taiwan)\nhttps://bitbucket.org/KuanHsu/poseidonfb\n\nlibreoffice-style-sifr ICONs\nBy Rizal Muttaqin\nhttps://github.com/rizmut/libreoffice-style-sifr\n" ~ ( GLOBAL.linuxHome.length ? "\nAppImage" : ""  ~ ( GLOBAL.iconv != null ? "\n*Using iconv Library" : "" ) ) ) );
 		version(DIDE)	IupMessage( GLOBAL.languageItems["about"].toCString, toStringz( "D Programming IDE\nPoseidonD(V0.077)  2022.07.03\nBy Kuan Hsu (Taiwan)\nhttps://bitbucket.org/KuanHsu/poseidonfb\n\nlibreoffice-style-sifr ICONs\nBy Rizal Muttaqin\nhttps://github.com/rizmut/libreoffice-style-sifr\n" ~ ( GLOBAL.linuxHome.length ? "\nAppImage" : ""  ~ ( GLOBAL.iconv != null ? "\n*Using iconv Library" : "" ) ) ) );
 		return IUP_DEFAULT;
 	});
@@ -2065,7 +2065,24 @@ extern(C)
 					{
 						if( tools.lowerCase( manualPath.ext ) == "chm" )
 						{
-							version(Windows) IupExecute( toStringz( splitWords[1] ), "" ); else IupExecute( "./CHMVIEW", toStringz( splitWords[1] ) );
+							version(Windows) 
+								IupExecute( toStringz( splitWords[1] ), "" );
+							else
+							{
+								scope htmlappPath = new FilePath( GLOBAL.linuxHtmlAppName );
+								switch( Util.trim( htmlappPath.file ) )
+								{
+									case "xchm":
+										IupExecute( toStringz( GLOBAL.linuxHtmlAppName ), toStringz( "\"" ~ splitWords[1] ~ "\"" ) );	// xchm "file:/home/username/freebasic/FB-manual-1.05.0.chm#xchm:/KeyPg%s.html"
+										break;
+									case "kchmviewer":
+									case "CHMVIEW":
+										IupExecute( toStringz( GLOBAL.linuxHtmlAppName ), toStringz( splitWords[1] ) );
+										break;
+									default:
+										IupExecute( "./CHMVIEW", toStringz( splitWords[1] ) );
+								}							
+							}
 						}
 					}
 					break;
